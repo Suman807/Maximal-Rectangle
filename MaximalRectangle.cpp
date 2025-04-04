5b1a4fad1335a59b20b20f08cf0ead954a2e03c7
Bhattacharjee <2374880@cognizant.com>
2025-04-04 16:40:21 +0530

    Tesing merge 2nd

--- MaximalRectangle.cpp
+++ MaximalRectangle.cpp
@@ -77,7 +77,7 @@ void histogram_stack(vector<int> &v, int &maxLen){
     
 }
 
-//stack  2 approaches
+//stack  2 approaches to remmember
 void histogram_stack2(vector<int> &v,int &maxLen){
     stack<int> st;
     int rs=0,h=0;
